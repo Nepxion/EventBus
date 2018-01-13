c222e46a9519ce42880e7ca1859f268c9ec985f3
Nepxion <1394997@qq.com>
2018-01-14 01:53:05 +0800

    发布到Maven中央仓库

---
+++ version.bat
@@ -0,0 +1,19 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                      Nepxion EventBus                     $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Technologies All Right Reserved                  $
+@echo $  Copyright(C) 2017                                        $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion EventBus
+@color 0a
+
+mvn versions:set -DnewVersion=1.0.0
+pause
