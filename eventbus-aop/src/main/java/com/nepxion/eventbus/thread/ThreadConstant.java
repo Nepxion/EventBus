2a9306a9874e461f450c56e9cb156d71431d0383
Nepxion <1394997@qq.com>
2018-01-17 01:24:48 +0800

    修改配置文件命名格式

--- eventbus-aop/src/main/java/com/nepxion/eventbus/thread/ThreadConstant.java
+++ eventbus-aop/src/main/java/com/nepxion/eventbus/thread/ThreadConstant.java
@@ -12,15 +12,15 @@ package com.nepxion.eventbus.thread;
 public class ThreadConstant {
     public static final int CPUS = Math.max(2, Runtime.getRuntime().availableProcessors());
 
-    public static final String THREAD_POOL_MULTI_MODE = "threadPool.multiMode";
-    public static final String THREAD_POOL_SHARED_NAME = "threadPool.sharedName";
-    public static final String THREAD_POOL_NAME_CUSTOMIZED = "threadPool.nameCustomized";
-    public static final String THREAD_POOL_NAME_IP_SHOWN = "threadPool.nameIPShown";
-    public static final String THREAD_POOL_CORE_POOL_SIZE = "threadPool.corePoolSize";
-    public static final String THREAD_POOL_MAXIMUM_POOL_SIZE = "threadPool.maximumPoolSize";
-    public static final String THREAD_POOL_KEEP_ALIVE_TIME = "threadPool.keepAliveTime";
-    public static final String THREAD_POOL_ALLOW_CORE_THREAD_TIMEOUT = "threadPool.allowCoreThreadTimeout";
-    public static final String THREAD_POOL_QUEUE = "threadPool.queue";
-    public static final String THREAD_POOL_QUEUE_CAPACITY = "threadPool.queueCapacity";
-    public static final String THREAD_POOL_REJECTED_POLICY = "threadPool.rejectedPolicy";
+    public static final String THREAD_POOL_MULTI_MODE = "threadpool.multi.mode";
+    public static final String THREAD_POOL_SHARED_NAME = "threadpool.shared.name";
+    public static final String THREAD_POOL_NAME_CUSTOMIZED = "threadpool.name.customized";
+    public static final String THREAD_POOL_NAME_IP_SHOWN = "threadpool.name.ipshown";
+    public static final String THREAD_POOL_CORE_POOL_SIZE = "threadpool.core.pool.size";
+    public static final String THREAD_POOL_MAXIMUM_POOL_SIZE = "threadpool.maximum.pool.size";
+    public static final String THREAD_POOL_KEEP_ALIVE_TIME = "threadpool.keep.alive.time";
+    public static final String THREAD_POOL_ALLOW_CORE_THREAD_TIMEOUT = "threadpool.allow.core.thread.timeout";
+    public static final String THREAD_POOL_QUEUE = "threadpool.queue";
+    public static final String THREAD_POOL_QUEUE_CAPACITY = "threadpool.queue.capacity";
+    public static final String THREAD_POOL_REJECTED_POLICY = "threadpool.rejected.policy";
 }
