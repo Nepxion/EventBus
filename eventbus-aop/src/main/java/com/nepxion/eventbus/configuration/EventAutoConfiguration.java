48c733d28678514876d8e02e987b11f988bd4aa8
Nepxion <1394997@qq.com>
2018-06-27 15:42:01 +0800

    升级版本

--- eventbus-aop/src/main/java/com/nepxion/eventbus/configuration/EventAutoConfiguration.java
+++ eventbus-aop/src/main/java/com/nepxion/eventbus/configuration/EventAutoConfiguration.java
@@ -27,7 +27,7 @@ public class EventAutoConfiguration {
         System.out.println("║╔══╣╚╝║║═╣╔╗╣║║╔═╗║║║║══╣");
         System.out.println("║╚══╬╗╔╣║═╣║║║╚╣╚═╝║╚╝╠══║");
         System.out.println("╚═══╝╚╝╚══╩╝╚╩═╩═══╩══╩══╝");
-        System.out.println("Nepxion EventBus  v1.0.13");
+        System.out.println("Nepxion EventBus  v1.0.14");
         System.out.println("");
     }
 
