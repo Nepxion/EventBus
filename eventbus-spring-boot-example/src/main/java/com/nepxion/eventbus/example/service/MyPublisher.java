bff837a411b20b14c30aaa648e60b8245501090d
Nepxion <1394997@qq.com>
2018-08-20 14:33:54 +0800

    增加@EnableEventBus开关和配置项

--- eventbus-spring-boot-example/src/main/java/com/nepxion/eventbus/example/service/MyPublisher.java
+++ eventbus-spring-boot-example/src/main/java/com/nepxion/eventbus/example/service/MyPublisher.java
@@ -21,31 +21,35 @@ import com.nepxion.eventbus.core.EventControllerFactory;
 public class MyPublisher {
     private static final Logger LOG = LoggerFactory.getLogger(MyPublisher.class);
 
-    @Autowired
+    @Autowired(required = false)
     private EventControllerFactory eventControllerFactory;
 
     public void publish() {
-        LOG.info("发送事件...");
+        if (eventControllerFactory != null) {
+            LOG.info("发送事件...");
 
-        // 异步模式下(默认)，子线程中收到派发的事件
-        eventControllerFactory.getAsyncController().post("Sync Event String Format");
+            // 异步模式下(默认)，子线程中收到派发的事件
+            eventControllerFactory.getAsyncController().post("Sync Event String Format");
 
-        // 同步模式下，主线程中收到派发的事件
-        // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
-        eventControllerFactory.getSyncController().post("Sync Event String Format");
+            // 同步模式下，主线程中收到派发的事件
+            // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
+            eventControllerFactory.getSyncController().post("Sync Event String Format");
 
-        // 异步模式下(默认)，子线程中收到派发的事件
-        eventControllerFactory.getAsyncController().post(12345L);
+            // 异步模式下(默认)，子线程中收到派发的事件
+            eventControllerFactory.getAsyncController().post(12345L);
 
-        // 同步模式下，主线程中收到派发的事件
-        // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
-        eventControllerFactory.getSyncController().post(Boolean.TRUE);
+            // 同步模式下，主线程中收到派发的事件
+            // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
+            eventControllerFactory.getSyncController().post(Boolean.TRUE);
 
-        // 异步模式下(默认)，子线程中收到派发的事件
-        eventControllerFactory.getAsyncController().postEvent(new Event("Async Event"));
+            // 异步模式下(默认)，子线程中收到派发的事件
+            eventControllerFactory.getAsyncController().postEvent(new Event("Async Event"));
 
-        // 同步模式下，主线程中收到派发的事件
-        // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
-        eventControllerFactory.getSyncController().postEvent(new Event("Sync Event"));
+            // 同步模式下，主线程中收到派发的事件
+            // 事件派发接口中eventControllerFactory.getSyncController(identifier)必须和@EnableEventBus参数保持一致，否则会收不到事件
+            eventControllerFactory.getSyncController().postEvent(new Event("Sync Event"));
+        } else {
+            LOG.error("EventBus功能未开启");
+        }
     }
 }
