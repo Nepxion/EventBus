145b19e7fe3ed2c91687868bc4c047125563f0d7
HaojunRen <1394997@qq.com>
2024-03-30 14:45:11 +0800

    Bump version to 3.0.0

--- version.bat
+++ version.bat
@@ -15,6 +15,6 @@
 @title Nepxion EventBus
 @color 0a
 
-call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=2.0.15
+call mvn versions:set -DgenerateBackupPoms=false -DnewVersion=3.0.0
 
 pause
